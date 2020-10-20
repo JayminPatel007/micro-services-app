ae5df0371e0760f110708c3dced3e84ea19d5fcb
discussion4all <discussion4all@yahoo.com>
2020-10-20 18:02:02 +0530

    Completed sign up, sign in pages

---
+++ client/pages/_app.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps}/>
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,16 @@
-export default () => {
-    return (
-        <h1>Landing page</h1>
-    )
-}
+import React from 'react';
+import buildClient from '../api/build-client';
+
+const LandingPage = ({currentUser}) => {
+
+    return currentUser ? <h1>Ypu are Signed In</h1> : <h1>You are NOT Signed In</h1>
+};
+
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+
+    return data;
+};
+
+export default LandingPage;
