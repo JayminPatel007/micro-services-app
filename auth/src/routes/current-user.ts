fde2f93d3be30178c2a8df95625d15d816c4a05f
discussion4all <discussion4all@yahoo.com>
2020-10-22 16:41:27 +0530

    Craeted saperate module for code reusability between different services

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@jaymintickets/common';
 
 const router = express.Router();
 
